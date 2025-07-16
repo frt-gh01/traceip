85441adc84eb0d53c9c2f6e0b79eab31baa4666d
Franciscello <ftarulla@gmail.com>
2025-07-16 18:37:39 -0300

    (WIP) Code refactor: move methodObject to simple object's method

--- src/main/java/org/example/IpTracer.java
+++ src/main/java/org/example/IpTracer.java
@@ -1,14 +1,20 @@
 package org.example;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Objects;
 
 public class IpTracer {
-    public IpTracer(String ipAddress) throws UnknownHostException {
+    public IpTracer() {
+    }
+
+    public InetAddress trace(String ipAddress)  throws UnknownHostException {
         Objects.requireNonNull(ipAddress, "Invalid IP Address: Null");
 
         if (ipAddress.isBlank()) {
             throw new UnknownHostException("Invalid IP Address: Empty");
         }
+
+        return InetAddress.getByName(ipAddress);
     }
 }
--- src/test/java/IpTracerTest.java
+++ src/test/java/IpTracerTest.java
@@ -1,20 +1,28 @@
 import org.example.IpTracer;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IpTracerTest {
+
+    IpTracer ipTracer;
+
+    @BeforeEach
+    void setUp() {
+        ipTracer = new IpTracer();
+    }
 
     @Test
     @DisplayName("Should not work with empty IP")
     void testIpTracerCreationWithEmptyIP() {
         Exception exception = assertThrows(UnknownHostException.class, () -> {
-            new IpTracer("  ");
+            ipTracer.trace("  ");
         });
 
         assertEquals("Invalid IP Address: Empty", exception.getMessage());
@@ -24,9 +32,21 @@ public class IpTracerTest {
     @DisplayName("Should not work with a NULL IP")
     void testIpTracerCreationWithNoIP() {
         Exception exception = assertThrows(NullPointerException.class, () -> {
-            new IpTracer(null);
+            ipTracer.trace(null);
         });
 
         assertEquals("Invalid IP Address: Null", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should work with a valid IP Address")
+    void testIpTracerCreationWithValidIP() {
+        String ipAddress = "192.168.0.1";
+
+        assertDoesNotThrow(() -> {
+            InetAddress address = ipTracer.trace(ipAddress);
+
+            assertEquals(ipAddress, address.getHostAddress());
+        });
     }
 }
